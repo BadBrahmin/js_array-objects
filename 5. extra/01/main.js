520028167533329ef9c50b17dffb4b222d459b61
BadBrahmin <tejasshree95@gmail.com>
2019-08-16 15:35:17 +0530

    add obj in arr

--- 5. extra/01/main.js
+++ 5. extra/01/main.js
@@ -1,1 +1,35 @@
-// Your solution
+// Your solution 
+
+
+//1
+
+function AnimalTestUser() {
+    username;
+
+    if (AnimalTestUser.length > 1) {
+        let otherArgs = [];
+
+    }
+}
+
+//2 Constructor
+
+var sheep = AnimalCreator('Cloud', 'sheep', 'You can count on me!', ['baahhh', 'arrgg', 'chewchewchew']);
+
+function AnimalCreator(username, species, tagline, noises) {
+    return {
+        username,
+        species,
+        tagline,
+        noises,
+    }
+}
+
+
+console.log(sheep);
+
+//3 addFriend
+
+
+
+
